8ea29958f4bc23c5b75742fa019736f40a2a79d6
Matt Hamil <j.matthew.hamil@gmail.com>
2017-09-24 16:54:25 -0500

    circle ci setup

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+now --public -e APP_ID=5413 \
+-e WEBHOOK_SECRET="$WEBHOOK_SECRET" \
+-e PRIVATE_KEY="$PRIVATE_KEY" \
+-e NODE_ENV=production
+
+now alias
+now rm --safe --yes react-navigation-bot
