e5a0d0d21ea78724fbcc8cabde4b4cd00753b1c0
Matt Hamil <j.matthew.hamil@gmail.com>
2017-09-24 20:13:12 -0500

    updated now deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,12 @@
 
 NOW=`pwd`/node_modules/.bin/now
 
+$NOW rm --yes react-navigation-bot.now.sh
+
 URL=$($NOW -t $NOW_TOKEN --public -e APP_ID="$APP_ID" \
 -e WEBHOOK_SECRET="$WEBHOOK_SECRET" \
 -e PRIVATE_KEY="$PRIVATE_KEY" \
 -e NODE_ENV=production)
 
 $NOW -t ${NOW_TOKEN} alias set ${URL} react-navigation-bot.now.sh
-now scale react-navigation-bot.now.sh 3
+$NOW scale react-navigation-bot.now.sh 3
