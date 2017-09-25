80ea87be971e40fa7b3f8241ccb21812a4379b8d
Matt Hamil <j.matthew.hamil@gmail.com>
2017-09-24 21:13:01 -0500

    fixed typo in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ URL=$($NOW -t $NOW_TOKEN --force --public -e APP_ID="$APP_ID" \
 -e WEBHOOK_SECRET="$WEBHOOK_SECRET" \
 -e PRIVATE_KEY="$PRIVATE_KEY" \
 -e NODE_ENV=production && sleep 5)
-$NOW -t ${NOW_TOKEN} alias set ${URL} react-navigation-bot.sh
+$NOW -t ${NOW_TOKEN} alias set ${URL} react-navigation-bot.now.sh
