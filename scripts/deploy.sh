502f7a38f8af15c486a02c247992da17be29c1f9
Matt Hamil <j.matthew.hamil@gmail.com>
2017-09-24 20:59:29 -0500

    fixed deployment scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,8 @@
 
 NOW=`pwd`/node_modules/.bin/now
 
-URL=$($NOW -t $NOW_TOKEN --public -e APP_ID="$APP_ID" \
+URL=$($NOW -t $NOW_TOKEN --force --public -e APP_ID="$APP_ID" \
 -e WEBHOOK_SECRET="$WEBHOOK_SECRET" \
 -e PRIVATE_KEY="$PRIVATE_KEY" \
--e NODE_ENV=production)
-
-$NOW -t ${NOW_TOKEN} alias set ${URL} react-navigation-bot.now.sh
-$NOW rm --safe --yes react-navigation-bot
+-e NODE_ENV=production && sleep 5)
+$NOW -t ${NOW_TOKEN} alias set ${URL} react-navigation-bot.sh
