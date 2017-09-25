0deca7c35418126374ffa93df102a5f1f3c8ac73
Matt Hamil <j.matthew.hamil@gmail.com>
2017-09-24 20:18:48 -0500

    fixed deployment config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,12 +2,9 @@
 
 NOW=`pwd`/node_modules/.bin/now
 
-$NOW -t ${NOW_TOKEN} rm --yes react-navigation-bot.now.sh
-
 URL=$($NOW -t $NOW_TOKEN --public -e APP_ID="$APP_ID" \
 -e WEBHOOK_SECRET="$WEBHOOK_SECRET" \
 -e PRIVATE_KEY="$PRIVATE_KEY" \
 -e NODE_ENV=production)
 
 $NOW -t ${NOW_TOKEN} alias set ${URL} react-navigation-bot.now.sh
-$NOW -t ${NOW_TOKEN} scale react-navigation-bot.now.sh 3
