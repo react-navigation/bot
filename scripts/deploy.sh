be3ccf0d568d6a086fa31cbdb1c7977a6e602e05
Matt Hamil <j.matthew.hamil@gmail.com>
2017-09-24 19:21:48 -0500

    updated now deploy config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-now --public -e APP_ID="$APP_ID" \
+NOW=`pwd`/node_modules/.bin/now
+
+$NOW -t $NOW_TOKEN --public -e APP_ID="$APP_ID" \
 -e WEBHOOK_SECRET="$WEBHOOK_SECRET" \
 -e PRIVATE_KEY="$PRIVATE_KEY" \
 -e NODE_ENV=production
 
-now alias
-now rm --safe --yes react-navigation-bot
+$NOW alias
+$NOW rm --safe --yes react-navigation-bot
